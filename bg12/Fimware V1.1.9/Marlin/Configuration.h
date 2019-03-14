397178d3d7537d4648136b3a337c4f1b20d447f0
John Link <john.link33@gmail.com>
2019-03-14 09:55:52 -0400

    updated axes limits

--- bg12/Fimware V1.1.9/Marlin/Configuration.h
+++ bg12/Fimware V1.1.9/Marlin/Configuration.h
@@ -874,8 +874,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 300
+#define Y_BED_SIZE 300
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -883,7 +883,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Z_MAX_POS 380
 
 /**
  * Software Endstops
