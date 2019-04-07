df8c285d697f5865d111d52f89873c13222f5694
JohnLink1 <john.link33@gmail.com>
2019-04-07 17:15:10 -0400

    updated firmware to allow for large prints

--- bg12/Fimware V1.1.9/Marlin/Configuration.h
+++ bg12/Fimware V1.1.9/Marlin/Configuration.h
@@ -440,7 +440,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 200
+#define EXTRUDE_MAXLENGTH 350
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
