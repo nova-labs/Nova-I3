991056c0f670fb98f26db3ee284899669ce589ae
JohnLink1 <john.link33@gmail.com>
2019-04-26 19:32:22 -0400

    Fixed currents, use M502 to load firmware after uploading, still testing temps and accelerations, DO NOT USE WITHOUT COOLING

--- bg12/Fimware V1.1.9/Marlin/Configuration_adv.h
+++ bg12/Fimware V1.1.9/Marlin/Configuration_adv.h
@@ -1102,13 +1102,13 @@
   #define HOLD_MULTIPLIER    0.5  // Scales down the holding current from run current
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
-  #define X_CURRENT          800  // rms current in mA. Multiply by 1.41 for peak current.
+  #define X_CURRENT          1000  // rms current in mA. Multiply by 1.41 for peak current.
   #define X_MICROSTEPS        16  // 0..256
 
-  #define Y_CURRENT          1200
+  #define Y_CURRENT          1400
   #define Y_MICROSTEPS        16
 
-  #define Z_CURRENT          1200
+  #define Z_CURRENT          1500
   #define Z_MICROSTEPS        16
 
   #define X2_CURRENT         800
