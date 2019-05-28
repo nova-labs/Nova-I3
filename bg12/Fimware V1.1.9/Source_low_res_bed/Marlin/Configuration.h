2b7cef5896881ca61b57d3b0d36ca7b70ba7cc43
JohnLink1 <john.link33@gmail.com>
2019-05-28 07:08:07 -0400

    I added all the precompiled hex files for the different bed types

--- bg12/Fimware V1.1.9/Source_low_res_bed/Marlin/Configuration.h
+++ bg12/Fimware V1.1.9/Source_low_res_bed/Marlin/Configuration.h
@@ -404,7 +404,7 @@
  * When set to any value below 255, enables a form of PWM to the bed that acts like a divider
  * so don't use it unless you are OK with PWM on your bed. (See the comment on enabling PIDTEMPBED)
  */
-#define MAX_BED_POWER 150 // limits duty cycle to bed; 255=full current
+#define MAX_BED_POWER 200 // limits duty cycle to bed; 255=full current
 
 #if ENABLED(PIDTEMPBED)
 
