b8570c4397028c6239b075d540d2a67dc85b77a1
paenian <paenian@gmail.com>
2020-08-11 12:17:06 -0400

    config

--- lab_machines_firmware/working/Marlin/Marlin/Configuration.h
+++ lab_machines_firmware/working/Marlin/Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(paenian, Eighty Two)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(paenian, Eighty Five)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Eighty-Two"
+#define CUSTOM_MACHINE_NAME "Eighty-Five is Alive"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
